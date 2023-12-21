22792ce5137f487025bbaee7b1007a192238ba91
Daniel <daniel@daniellaing.com>
2023-12-21 15:13:25 +0000

    Basic routing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,16 +1,8 @@
-use axum::{routing::get, Router};
-use webr::{get_page, init, prelude::*};
+use webr::{init_app, prelude::*};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    init()?;
-
-    let app = Router::new().route("/", get(|| async { "Hello world!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-
-    let page = get_page(String::from("Test page"));
-    println!("{page}");
-
+    let (listener, app) = init_app().await?;
+    axum::serve(listener, app).await?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,24 @@ mod utils;
 
 use std::collections::HashMap;
 
+use axum::{response::IntoResponse, routing::get, Router};
+use tokio::net::TcpListener;
+
 use crate::prelude::*;
 
-pub fn init() -> Result<()> {
+pub async fn init_app() -> Result<(TcpListener, Router)> {
     env_logger::init();
+    let app = init_router().await;
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
 
-    Ok(())
+    log::info!("Listening on http://{}", listener.local_addr()?);
+    Ok((listener, app))
 }
 
-pub fn get_page(str: String) -> String {
-    format!("Page placeholder.\n{str}")
+async fn init_router() -> Router<()> {
+    Router::new().route("/", get(get_page))
+}
+
+async fn get_page() -> String {
+    String::from("Placeholder")
 }
