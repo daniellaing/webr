56eb727ca0dd6860aba3359d4dc42ea41c70a074
Daniel Laing <daniel@daniellaing.com>
2025-08-15 17:26:40 +0100

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -1,53 +1,62 @@
 {
   description = "A Rust project";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
     nixpkgs,
-    flake-utils,
+    flake-parts,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      deploy = pkgs.writeShellApplication {
-        name = "deploy";
-        runtimeInputs = with pkgs; [openssh];
-        text = ''
-          ssh root@daniellaing.com <<'EOF'
-            rm -rf /tmp/webr
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-            git clone /home/git/webr.git /tmp/webr
-            cd /tmp/webr
-            cargo install --path . --root /usr/local
+      perSystem = {pkgs, ...}: let
+        deploy = pkgs.writeShellApplication {
+          name = "deploy";
+          runtimeInputs = with pkgs; [openssh];
+          text = ''
+            ssh root@daniellaing.com <<'EOF'
+              rm -rf /tmp/webr
 
-            systemctl restart webr.service
-          EOF
-        '';
+              git clone /home/git/webr.git /tmp/webr
+              cd /tmp/webr
+              cargo install --path . --root /usr/local
+
+              systemctl restart webr.service
+            EOF
+          '';
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            deploy
+
+            rustc
+            cargo
+            rust-analyzer
+            rustfmt
+            clippy
+            cargo-watch
+            cargo-edit
+
+            prettierd
+
+            # Markdown
+            proselint
+            marksman
+            markdownlint-cli2
+          ];
+
+          shellHook = ''
+            echo "Let's get Rusty" | "${pkgs.lolcat}/bin/lolcat"
+          '';
+        };
       };
-    in {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [
-          deploy
-
-          rustc
-          cargo
-          rust-analyzer
-          rustfmt
-          clippy
-          cargo-watch
-
-          prettierd
-
-          # Markdown
-          proselint
-          marksman
-          markdownlint-cli2
-        ];
-
-        shellHook = ''
-          echo "Let's get Rusty" | "${pkgs.lolcat}/bin/lolcat"
-        '';
-      };
-    });
+    };
 }
